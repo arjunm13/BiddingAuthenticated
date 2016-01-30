5908ab2f02822dbb1278bb05181448d1eff386f9
arjunm13 <arjunmahen13@gmail.com>
2016-01-29 20:46:16 -0500

    changes view cases

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	//'url' : 'mongodb://arjun:Yoyo1990@ds051625.mongolab.com:51625/bidding'
-	'url' : 'mongodb://localhost:27017/bidding'
+	'url' : 'mongodb://arjun:Yoyo1990@ds051625.mongolab.com:51625/bidding'
+	//'url' : 'mongodb://localhost:27017/bidding'
 }
